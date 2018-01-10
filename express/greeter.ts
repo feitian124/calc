c528e303ce5def6d703f25465cdf2611b9d39db3
ypeng <yun.peng@fisglobal.com>
2018-01-10 21:08:06 +0800

    add npm

---
+++ express/greeter.ts
@@ -0,0 +1,12 @@
+function greeter(person: string) {
+  return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+document.body.innerHTML = greeter(user);
+
+// http://www.jb51.net/article/53537.htm
+// http://blog.csdn.net/nkuhjp/article/details/53365036
+// http://blog.csdn.net/LoveLion/article/details/7713644
+// https://medium.freecodecamp.org/parsing-math-expressions-with-javascript-7e8f5572276e
