38714ceca69dd57ae65ef55f8a1dceac588de73b
ypeng <yun.peng@fisglobal.com>
2018-01-12 12:57:58 +0800

    add toRNP

--- math-parser/Parser.ts
+++ math-parser/Parser.ts
@@ -9,8 +9,93 @@ export default class Parser {
   inputs: Stack;
   outputs: Stack;
 
-  constructor(message: string) {
+  constructor() {
     this.inputs = new Stack();
     this.outputs = new Stack();
   }
+
+  isOperator(value){
+    var operatorString = "+-*/()";
+    return operatorString.indexOf(value) > -1
 }
+
+getPrioraty(value){
+    switch(value){
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        default:
+            return 0;
+    }
+}
+
+prioraty(o1, o2){
+    return this.getPrioraty(o1) <= this.getPrioraty(o2);
+}
+
+dal2Rpn(exp){
+    var inputStack = [];
+    var outputStack = [];
+    var outputQueue = [];
+
+    for(var i = 0, len = exp.length; i < len; i++){
+        var cur = exp[i];
+        if(cur != ' ' ){
+            inputStack.push(cur);
+        }
+    }
+    console.log('step one');
+    while(inputStack.length > 0){
+        var cur = inputStack.shift();
+        if(this.isOperator(cur)){
+            if(cur == '('){
+                outputStack.push(cur);
+            }else if(cur == ')'){
+                var po = outputStack.pop();
+                while(po != '(' && outputStack.length > 0){
+                    outputQueue.push(po);
+                    po = outputStack.pop();
+                }
+                if(po != '('){
+                    throw "error: unmatched ()";
+                }
+            }else{
+                while(this.prioraty(cur, outputStack[outputStack.length - 1]) && outputStack.length > 0){
+                    outputQueue.push(outputStack.pop());
+                }
+                outputStack.push(cur);
+            }
+        }else{
+            outputQueue.push(new Number(cur));
+        }
+    }
+    console.log('step two');
+    if(outputStack.length > 0){
+        if(outputStack[outputStack.length - 1] == ')' || outputStack[outputStack.length - 1] == '('){
+            throw "error: unmatched ()";
+        }
+        while(outputStack.length > 0){
+            outputQueue.push(outputStack.pop());
+        }
+    }
+    console.log('step three');
+    return outputQueue;
+  }
+
+  test() {
+    console.log(this.dal2Rpn('1 + 2'));
+    console.log(this.dal2Rpn('1 + 2 + 3'));
+    console.log(this.dal2Rpn('1 + 2 * 3'));
+    console.log(this.dal2Rpn('1 + 2 * 3 - 4 / 5'));
+    console.log(this.dal2Rpn('( 1 + 2 )'));
+
+    console.log(this.dal2Rpn('( 1 + 2 ) * ( 3 - 4 ) / 5'));
+    console.log(this.dal2Rpn('( 1 + 2 ) * (( 3 - 4 ) / 5)'));
+  }
+}
+
+let parser = new Parser();
+parser.test();
---
+++ math-parser/test/Parser.test.ts
@@ -0,0 +1,13 @@
+import Parser from '../Parser';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Parser', () => {
+  it('push one value to Parser, can peek the value, and the Parser should not be empty', () => {
+    let parser = new Parser();
+  });
+
+  it('push one value to Parser, can pop the value, and the Parser should be  empty', () => {
+    let parser = new Parser();
+  });
+});
