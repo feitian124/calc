133bccf052d7e07303e62016c502170777e574da
ypeng <yun.peng@fisglobal.com>
2018-01-15 20:59:04 +0800

    move methods to operator

--- math-parser/Parser.ts
+++ math-parser/Parser.ts
@@ -1,9 +1,7 @@
-// http://www.jb51.net/article/53537.htm
-// http://blog.csdn.net/nkuhjp/article/details/53365036
-// http://blog.csdn.net/LoveLion/article/details/7713644
-// https://medium.freecodecamp.org/parsing-math-expressions-with-javascript-7e8f5572276e
+
 
 import Stack from "./Stack";
+import Operator from "./Operator";
 
 export default class Parser {
   inputs: Stack;
@@ -13,28 +11,6 @@ export default class Parser {
     this.inputs = new Stack();
     this.outputs = new Stack();
   }
-
-  isOperator(value){
-    var operatorString = "+-*/()";
-    return operatorString.indexOf(value) > -1
-}
-
-getPrioraty(value){
-    switch(value){
-        case '+':
-        case '-':
-            return 1;
-        case '*':
-        case '/':
-            return 2;
-        default:
-            return 0;
-    }
-}
-
-prioraty(o1, o2){
-    return this.getPrioraty(o1) <= this.getPrioraty(o2);
-}
 
 dal2Rpn(exp){
     var inputStack = [];
@@ -51,7 +27,7 @@ dal2Rpn(exp){
     }
     while(inputStack.length > 0){
         var cur = inputStack.shift();
-        if(this.isOperator(cur)){
+        if(Operator.isOperator(cur)){
             if(cur == '('){
                 outputStack.push(cur);
             }else if(cur == ')'){
@@ -64,7 +40,7 @@ dal2Rpn(exp){
                     throw "error: unmatched ()";
                 }
             }else{
-                while(this.prioraty(cur, outputStack[outputStack.length - 1]) && outputStack.length > 0){
+                while(Operator.prioraty(cur, outputStack[outputStack.length - 1]) && outputStack.length > 0){
                     outputQueue.push(outputStack.pop());
                 }
                 outputStack.push(cur);
@@ -89,7 +65,7 @@ dal2Rpn(exp){
     while(rpnQueue.length > 0){
         var cur = rpnQueue.shift();
 
-        if(!this.isOperator(cur)){
+        if(!Operator.isOperator(cur)){
             outputStack.push(cur);
         }else{
             if(outputStack.length < 2){
