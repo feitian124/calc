89955f3096e5ff414e49d3909e2c3662e8301261
ypeng <yun.peng@fisglobal.com>
2018-01-15 21:22:25 +0800

    move test to test

--- math-parser/Parser.ts
+++ math-parser/Parser.ts
@@ -1,90 +1,80 @@
 import Operator from "./Operator";
 
 export default class Parser {
+  outputQueue: any[] = [];
 
-dal2Rpn(exp){
+  rpn(exp) {
     let inputStack = [];
     let outputStack = [];
-    let outputQueue = [];
 
-    for(let i = 0, len = exp.length; i < len; i++){
-        let cur = exp[i];
-        if(cur != ' ' ){
-            inputStack.push(cur);
-        }
+    for (let i = 0, len = exp.length; i < len; i++) {
+      let cur = exp[i];
+      if (cur != ' ') {
+        inputStack.push(cur);
+      }
     }
-    while(inputStack.length > 0){
-        let cur = inputStack.shift();
-        if(Operator.isOperator(cur)){
-            if(cur == '('){
-                outputStack.push(cur);
-            }else if(cur == ')'){
-                let po = outputStack.pop();
-                while(po != '(' && outputStack.length > 0){
-                    outputQueue.push(po);
-                    po = outputStack.pop();
-                }
-                if(po != '('){
-                    throw "error: unmatched ()";
-                }
-            }else{
-                while(Operator.prioraty(cur, outputStack[outputStack.length - 1]) && outputStack.length > 0){
-                    outputQueue.push(outputStack.pop());
-                }
-                outputStack.push(cur);
-            }
-        }else{
-            outputQueue.push(new Number(cur));
-        }
-    }
-    if(outputStack.length > 0){
-        if(outputStack[outputStack.length - 1] == ')' || outputStack[outputStack.length - 1] == '('){
+    while (inputStack.length > 0) {
+      let cur = inputStack.shift();
+      if (Operator.isOperator(cur)) {
+        if (cur == '(') {
+          outputStack.push(cur);
+        } else if (cur == ')') {
+          let po = outputStack.pop();
+          while (po != '(' && outputStack.length > 0) {
+            this.outputQueue.push(po);
+            po = outputStack.pop();
+          }
+          if (po != '(') {
             throw "error: unmatched ()";
+          }
+        } else {
+          while (Operator.prioraty(cur, outputStack[outputStack.length - 1]) && outputStack.length > 0) {
+            this.outputQueue.push(outputStack.pop());
+          }
+          outputStack.push(cur);
         }
-        while(outputStack.length > 0){
-            outputQueue.push(outputStack.pop());
-        }
+      } else {
+        this.outputQueue.push(new Number(cur));
+      }
     }
-    return outputQueue;
+    if (outputStack.length > 0) {
+      if (outputStack[outputStack.length - 1] == ')' || outputStack[outputStack.length - 1] == '(') {
+        throw "error: unmatched ()";
+      }
+      while (outputStack.length > 0) {
+        this.outputQueue.push(outputStack.pop());
+      }
+    }
+    return this;
   }
 
-  evalRpn(rpnQueue){
+  result() {
     let outputStack = [];
-    while(rpnQueue.length > 0){
-        let cur = rpnQueue.shift();
+    while (this.outputQueue.length > 0) {
+      let cur = this.outputQueue.shift();
 
-        if(!Operator.isOperator(cur)){
-            outputStack.push(cur);
-        }else{
-            if(outputStack.length < 2){
-                throw "unvalid stack length";
-            }
-            let sec = outputStack.pop();
-            let fir = outputStack.pop();
-
-            outputStack.push(this.getResult(fir, sec, cur));
+      if (!Operator.isOperator(cur)) {
+        outputStack.push(cur);
+      } else {
+        if (outputStack.length < 2) {
+          throw "unvalid stack length";
         }
+        let sec = outputStack.pop();
+        let fir = outputStack.pop();
+
+        outputStack.push(this.getResult(fir, sec, cur));
+      }
     }
 
-    if(outputStack.length != 1){
-        throw "unvalid expression";
-    }else{
-        return outputStack[0];
+    if (outputStack.length != 1) {
+      throw "unvalid expression";
+    } else {
+      return outputStack[0];
     }
-}
+  }
 
-getResult(fir: Number, sec: Number, cur) : Number {
-  let exp: string = fir + cur + sec;
-  return eval(exp);
-}
-
-test() {
-    console.log(this.evalRpn(this.dal2Rpn('1 + 2')));
-    console.log(this.evalRpn(this.dal2Rpn('1 + 2 + 3')));
-    console.log(this.evalRpn(this.dal2Rpn('1 + 2 * 3')));
-    console.log(this.evalRpn(this.dal2Rpn('1 + 2 * 3 - 4 / 5')));
-    console.log(this.evalRpn(this.dal2Rpn('( 1 + 2 )')));
-    console.log(this.evalRpn(this.dal2Rpn('( 1 + 2 ) * ( 3 - 4 ) / 5')));
-    console.log(this.evalRpn(this.dal2Rpn('( 1 + 2 ) * (( 3 - 4 ) / 5)')));
+  getResult(fir: Number, sec: Number, cur): Number {
+    let exp: string = fir + cur + sec;
+    return eval(exp);
   }
 }
--- math-parser/test/Parser.test.ts
+++ math-parser/test/Parser.test.ts
@@ -5,8 +5,30 @@ import 'mocha';
 describe('Parser', () => {
   let parser = new Parser();
   it('1+2', () => {
-    let rpn = parser.dal2Rpn('1+2');
-    let result = parser.evalRpn(rpn);
-    expect(result).to.equal(3);
+    expect(parser.rpn('1+2').result()).to.equal(3);
+  });
+
+  it('1 + 2 + 3', () => {
+    expect(parser.rpn('1 + 2 + 3').result()).to.equal(6);
+  });
+
+  it('1 + 2 * 3', () => {
+    expect(parser.rpn('1 + 2 * 3').result()).to.equal(7);
+  });
+
+  it('1 + 2 * 3 - 4 / 5', () => {
+    expect(parser.rpn('1 + 2 * 3 - 4 / 5').result()).to.equal(6.2);
+  });
+
+  it('( 1 + 2 )', () => {
+    expect(parser.rpn('( 1 + 2 )').result()).to.equal(3);
+  });
+
+  it('( 1 + 2 ) * ( 3 - 4 ) / 5', () => {
+    expect(parser.rpn('( 1 + 2 ) * ( 3 - 4 ) / 5').result()).to.equal(-0.6);
+  });
+
+  it('( 1 + 2 ) * (( 3 - 4 ) / 5)', () => {
+    expect(parser.rpn('( 1 + 2 ) * (( 3 - 4 ) / 5)').result()).to.equal(-0.6000000000000001);
   });
 });
